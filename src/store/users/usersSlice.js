8eb9bf0a72509d4a133303b3324e12745c65798b
Momo-87 <momochristian87@gmail.com>
2023-06-14 10:32:05 +0100

    Configure linters

--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    users: [],
-    isLoading: true,
-    error: undefined,
+  users: [],
+  isLoading: true,
+  error: undefined,
 };
 
 const usersSlice = createSlice({
